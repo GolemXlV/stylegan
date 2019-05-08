0ac1b0b140cedbd3af18a9899dd7ee5fc080b03b
golemxiv <shaper2010@gmail.com>
2019-05-08 22:29:15 +0400

    update image dir

--- train.py
+++ train.py
@@ -34,7 +34,7 @@ if 1:
     tf_config     = {'rnd.np_random_seed': 1000}                                           # Options for tflib.init_tf().
 
     # Dataset.
-    desc += '-images_512x512';     dataset = EasyDict(tfrecord_dir='/dltraining/projects/style_gan/datasets/images_512x512'); train.mirror_augment = True
+    desc += '-images_512x512';     dataset = EasyDict(tfrecord_dir='images_512x512'); train.mirror_augment = True
     # desc += '-ffhq';     dataset = EasyDict(tfrecord_dir='ffhq');                 train.mirror_augment = True
     #desc += '-ffhq512';  dataset = EasyDict(tfrecord_dir='ffhq', resolution=512); train.mirror_augment = True
     #desc += '-ffhq256';  dataset = EasyDict(tfrecord_dir='ffhq', resolution=256); train.mirror_augment = True
