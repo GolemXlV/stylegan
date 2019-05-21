2246c6708295feca05471e33299126d89f305fc8
golemxiv <shaper2010@gmail.com>
2019-05-22 02:00:22 +0400

    update learning rate

--- train.py
+++ train.py
@@ -47,13 +47,13 @@ if 1:
     #desc += '-1gpu'; submit_config.num_gpus = 1; sched.minibatch_base = 4; sched.minibatch_dict = {4: 128, 8: 128, 16: 128, 32: 64, 64: 32, 128: 16, 256: 8, 512: 4}
     #desc += '-2gpu'; submit_config.num_gpus = 2; sched.minibatch_base = 8; sched.minibatch_dict = {4: 256, 8: 256, 16: 128, 32: 64, 64: 32, 128: 16, 256: 8}
     #desc += '-4gpu'; submit_config.num_gpus = 4; sched.minibatch_base = 16; sched.minibatch_dict = {4: 512, 8: 256, 16: 128, 32: 64, 64: 32, 128: 16}
+    # desc += '-8gpu'; submit_config.num_gpus = 8; sched.minibatch_base = 32; sched.minibatch_dict = {4: 512, 8: 256, 16: 128, 32: 64, 64: 32}
     desc += '-8gpu'; submit_config.num_gpus = 8; sched.minibatch_base = 32; sched.minibatch_dict = {4: 512, 8: 256, 16: 128, 32: 64, 64: 32}
-    # desc += '-8gpu'; submit_config.num_gpus = 8; sched.minibatch_base = 32; sched.minibatch_dict = {4: 512, 8: 256, 16: 128, 32: 128, 64: 64}
 
     # Default options.
     train.total_kimg = 25000
     sched.lod_initial_resolution = 8
-    sched.G_lrate_dict = {128: 0.0015, 256: 0.002, 512: 0.003, 1024: 0.003}
+    sched.G_lrate_dict = {128: 0.00015, 256: 0.0002, 512: 0.0003, 1024: 0.0003}
     # sched.G_lrate_dict = {32: 0.0015, 128: 0.0015, 256: 0.002, 512: 0.003, 1024: 0.003}
     sched.D_lrate_dict = EasyDict(sched.G_lrate_dict)
 
@@ -95,7 +95,7 @@ def main():
     kwargs.submit_config.run_desc = desc
     kwargs.save_weight_histograms = True
     kwargs.network_snapshot_ticks = 5
-    kwargs.update(resume_run_id=-1)
+    kwargs.update(resume_run_id=10)
     dnnlib.submit_run(**kwargs)
 
 #----------------------------------------------------------------------------
